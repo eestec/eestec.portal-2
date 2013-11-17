53dfc0df082eae07484272b2014fc947c8721f9d
Erik Plestenjak <erik.plestenjak@gmail.com>
2013-11-17 02:39:00 +0100

    Implemented LC editing permissions (filtering by lc meta) and event editing permissions (only lc board do that, filtering by meta)

--- wp-content/plugins/EESTEC-permissions/EESTEC-permissions.php
+++ wp-content/plugins/EESTEC-permissions/EESTEC-permissions.php
@@ -38,26 +38,110 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 define('EESTEC_PERMISSIONS_VERSION', '1.0');
 define('EESTEC_PERMISSIONS_PLUGIN_URL', plugin_dir_url( __FILE__ ));
 
-/*
-if ( is_admin() )
-	require_once dirname( __FILE__ ) . '/admin.php';
-*/
 
-add_action('init', 'akismet_init');
+//give int board access to editing menu
+//give lc board permssion to add events
+//automate locking meta of lc on events
+//store organizer lc on the event
+//restrict user editing to same lc
+//correct the user confirmation plugin to support can user edit (confirming only editable users)
+//remap all events
 
-add_filter( 'posts_where' , 'posts_where' );
-
-function posts_where( $where ) {
-	if( is_admin() ) {
-		global $wpdb;
-		
-		if ( isset( $_GET['author_restrict_posts'] ) && !empty( $_GET['author_restrict_posts'] ) && intval( $_GET['author_restrict_posts'] ) != 0 ) {
-			$author = intval( $_GET['author_restrict_posts'] );
-		
-			$where .= "AND ID IN (SELECT object_id FROM {$wpdb->term_relationships} WHERE term_taxonomy_id=$author )";
-		}
-	}
-	return $where;
+function get_current_user_roles(){
+	global $current_user;
+	return $current_user->roles;
+	return $user_roles;
 }
 
-add_action( $hook, $function_to_add);
+function current_user_has_role($role)
+{
+	return in_array($role,get_current_user_roles());
+}
+
+add_filter( 'user_has_cap', 'edit_lcs', 100, 3 );
+function edit_lcs($allcaps, $cap, $args)
+{	
+	global $current_user;
+	if(array_key_exists('manage_options',$allcaps))//user is admin
+		return $allcaps;
+		
+	//print_r($cap);
+	//print_r($args);
+	
+	if(get_post_type()=='lcs'){		
+		if(current_user_has_role('intboard'))
+			return $allcaps;
+		     
+		$allcaps['delete_post'] = false;
+		$allcaps['edit_lcss'] = false; //disable adding new lcs.
+		
+		
+		
+		if($args[0]=='edit_post')
+		{
+		$lc=get_user_meta($current_user->ID,'lc',true);    //only members of the LC can edit the lc
+		if($args[2]!=$lc)
+			$allcaps[$cap[0]] = false;
+			}}		
+			
+	return $allcaps;
+}
+
+add_filter( 'user_has_cap', 'edit_users', 100, 3 );//restricts editable users to same LC only
+function edit_users($allcaps, $cap, $args)
+{	
+	global $current_user;
+	if(array_key_exists('manage_options',$allcaps))//user is admin
+		return $allcaps;
+
+	//print_r($cap);
+	//print_r($args);
+	
+	if($args[0]=='edit_user')
+	if(get_user_meta($args[2],'lc')!=get_user_meta($current_user->ID,'lc'))
+		$allcaps['edit_users']=false;
+	return $allcaps;
+}
+
+add_action('save_post', 'link_event_to_lc');
+function link_event_to_lc($post_id)
+{
+
+	if(get_post_type($post_id)=='events'){
+		print_r($post_id);
+		$meta_value=get_user_meta(get_current_user_id(),'lc',true);
+		add_post_meta($post_id,'lc', $meta_value,true);
+		}	
+}
+
+add_filter( 'user_has_cap', 'edit_events', 100, 3 );
+
+function edit_events($allcaps, $cap, $args)
+{	
+	global $current_user;
+	//if(array_key_exists('manage_options',$allcaps))//user is admin
+	//	return $allcaps;
+	
+	//print_r($cap);
+	//print_r($args);
+	
+	if(get_post_type()=='events'){     
+		$allcaps['delete_post'] = false;
+		
+		if($args[0]=='edit_post')
+		{
+		$lc=get_user_meta($current_user->ID,'lc',true);    //only members of the LC can edit the lc
+		if( get_post_meta($args[2],'lc',true)!=$lc)
+			$allcaps[$cap[0]] = false;
+			}}		
+			
+	return $allcaps;
+}
+
+add_action( 'restrict_manage_posts','hide_other_events');
+function hide_other_events($test)
+{
+		print_r($test);
+}
+
+?>
