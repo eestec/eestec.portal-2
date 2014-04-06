4fd40a0ea69c667a52f3ca935e0cdda5d5d03d2d
Erik Plestenjak <erik.plestenjak@gmail.com>
2014-04-07 00:21:19 +0200

    Removed clashing email form css.

--- wp-content/plugins/email-newsletter/widget/bootstrap_widget.php
+++ wp-content/plugins/email-newsletter/widget/bootstrap_widget.php
@@ -7,10 +7,10 @@
   <div class="eemail_msg">
     <span id="eemail_msg"></span>
   </div>
-  <div class="col-md-6 eemail_textbox">
-    <input class="eemail_textbox_class" name="eemail_txt_email" id="eemail_txt_email" onkeypress="if(event.keyCode==13) eemail_submit_ajax('<?php echo emailnews_plugin_url('widget'); ?>')" onblur="if(this.value=='') this.value='<?php echo get_option('eemail_widget_txt_cap'); ?>';" onfocus="if(this.value=='<?php echo get_option('eemail_widget_txt_cap'); ?>') this.value='';" value="<?php echo get_option('eemail_widget_txt_cap'); ?>" maxlength="150" type="text">
+  <div class="col-md-6">
+    <input  name="eemail_txt_email" id="eemail_txt_email" onkeypress="if(event.keyCode==13) eemail_submit_ajax('<?php echo emailnews_plugin_url('widget'); ?>')" onblur="if(this.value=='') this.value='<?php echo get_option('eemail_widget_txt_cap'); ?>';" onfocus="if(this.value=='<?php echo get_option('eemail_widget_txt_cap'); ?>') this.value='';" value="<?php echo get_option('eemail_widget_txt_cap'); ?>" maxlength="150" type="text">
   </div>
-  <div class="col-md-6 eemail_button">
-    <input class="eemail_textbox_button btn btn-red" name="eemail_txt_Button" id="eemail_txt_Button" onClick="return eemail_submit_ajax('<?php echo emailnews_plugin_url('widget'); ?>')" value="<?php echo get_option('eemail_widget_but_cap'); ?>" type="button">
+  <div class="col-md-6">
+    <input class="btn btn-red" name="eemail_txt_Button" id="eemail_txt_Button" onClick="return eemail_submit_ajax('<?php echo emailnews_plugin_url('widget'); ?>')" value="<?php echo get_option('eemail_widget_but_cap'); ?>" type="button">
   </div>
 </div>
