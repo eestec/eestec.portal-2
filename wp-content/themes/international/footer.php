ff5ab71cb5f78d547df74d692b2f0612bb71162c
Erik Plestenjak <erik.plestenjak@gmail.com>
2014-04-06 17:58:34 +0200

    Hidden map on xs devices.

--- wp-content/themes/international/footer.php
+++ wp-content/themes/international/footer.php
@@ -15,7 +15,7 @@
                     <div class="container">
                         <div class="row">
                         
-                        <div class="col-md-5 hidden-sm">
+                        <div class="col-md-5 hidden-sm hidden-xs">
                             <img src="<?php echo get_template_directory_uri(); ?>/images/footer_map.png" />
                         </div>
                         <div class="col-md-7">
